42fe06f6762d1153caa25db03b5adb65bc8aebda
umerhasan17 <umerhasan17@gmail.com>
2018-09-06 14:12:36 +0100

    Finished part 1 with everything operational

--- DRF Tutorial2/tutorial/snippets/views.py
+++ DRF Tutorial2/tutorial/snippets/views.py
@@ -8,6 +8,24 @@ from snippets.serializers import SnippetSerializer
 
 # Create your views here.
 @csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
 def snippet_detail(request, pk):
     """
     Retrieve, update or delete a code snippet.
