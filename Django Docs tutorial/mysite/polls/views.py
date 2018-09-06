1a098ca277e8a87955d19985d539a11abfb05638
umerhasan17 <umerhasan17@gmail.com>
2018-09-06 10:54:43 +0100

    Running tests in the shell and updating view to show publication date

--- Django Docs tutorial/mysite/polls/views.py
+++ Django Docs tutorial/mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice , Question
 
@@ -14,8 +15,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
