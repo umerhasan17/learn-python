9c7cd68398b0031d96a8a4de337d53f17f970b4d
umerhasan17 <umerhasan17@gmail.com>
2018-09-06 14:37:29 +0100

    Adding save method

--- DRF Tutorial2/tutorial/snippets/models.py
+++ DRF Tutorial2/tutorial/snippets/models.py
@@ -19,5 +19,18 @@ class Snippet(models.Model):
     owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
     highlighted = models.TextField()
 
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
+
     class Meta:
         ordering = ('created',)
