684307d4111ce53363d20aa0ac073f7c3c90a261
umerhasan17 <umerhasan17@gmail.com>
2018-09-06 14:57:37 +0100

    Adding object level permissions

--- DRF Tutorial2/tutorial/snippets/views.py
+++ DRF Tutorial2/tutorial/snippets/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer , UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 from rest_framework import status,  mixins,  generics, permissions
 from rest_framework.decorators import api_view
@@ -24,7 +25,8 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
