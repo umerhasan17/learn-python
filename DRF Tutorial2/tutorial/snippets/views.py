48c188d9b6e64fec2c4b4f4ba6ddd49221a6b637
umerhasan17 <umerhasan17@gmail.com>
2018-09-06 14:22:28 +0100

    Adding imports to views

--- DRF Tutorial2/tutorial/snippets/views.py
+++ DRF Tutorial2/tutorial/snippets/views.py
@@ -5,6 +5,9 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 @api_view(['GET', 'POST'])
