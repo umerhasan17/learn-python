0172188dacaec3631c98aaccde72848a2d2cfd84
umerhasan17 <umerhasan17@gmail.com>
2018-09-06 10:42:23 +0100

    undefined generic solved

--- Django Docs tutorial/mysite/polls/views.py
+++ Django Docs tutorial/mysite/polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
 from django.urls import reverse
-#from django.views.generic import TemplateView
+from django.views import generic
 
 from .models import Choice , Question
 
